cd0ef7b05fb314ec9e7491a0aa2bcb74eca402ac
Gilda Snow <gilda.snow@gmail.com>
2019-06-04 21:09:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,7 @@ def oxford_comma(array)
     puts "#{array[0]} and #{array[1]}"
     
   elsif 2 < array.length 
-      array[-1].insert(0, "and ").join(", ") 
+      array[-1].insert(0, "and ") 
     end
+    array.join(", ")
   end
